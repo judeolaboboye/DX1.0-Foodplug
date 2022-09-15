29785805c4aa2a57809502e0bded434b6c1c0e20
judeolaboboye <judeolaboboye@gmail.com>
2022-09-15 17:48:56 -0400

    Updated Foodplug

---
+++ starter/main.js
@@ -0,0 +1,32 @@
+
+
+// basic ES5 function declaration
+// funtion name(){}
+
+var myname = "Nirvana Studios"
+
+function popsomething() {
+alert( myname + "i have been coding since 12")
+}
+
+
+
+
+console.log(document)
+
+var ourbody = document.querySelector(".ourpagebody")
+
+// ourbody.style.background = "black"
+
+
+
+var hamburger = document.querySelector(".hamburger-container")
+
+console.log(hamburger)
+
+// hamburger.classlist.add(yam)
+
+
+function addandremove() {
+    hamburger.classList.toggle('showburger')
+}
